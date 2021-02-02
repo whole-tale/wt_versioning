14b3d15eb1e9a1a4d1608cd1a70d3e44470d8de2
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2021-02-02 10:34:52 -0600

    Drop unused method

--- server/lib/util.py
+++ server/lib/util.py
@@ -1,9 +1,7 @@
 import pathlib
 
 from girder.models.setting import Setting
-from girder.plugins.wt_home_dir.lib.PathMapper import TalePathMapper
 from ..constants import PluginSettings
-from girder.plugins.wt_home_dir.constants import PluginSettings as WTHomesPluginSettings
 
 
 def getTaleVersionsDirPath(tale: dict) -> pathlib.Path:
@@ -12,13 +10,6 @@ def getTaleVersionsDirPath(tale: dict) -> pathlib.Path:
 
 def getTaleRunsDirPath(tale: dict) -> pathlib.Path:
     return getTaleDirPath(tale, PluginSettings.RUNS_DIRS_ROOT)
-
-
-def getTaleWorkspaceDirPath(tale: dict) -> pathlib.Path:
-    settings = Setting()
-    root = settings.get(WTHomesPluginSettings.TALE_DIRS_ROOT)
-    taleId = str(tale['_id'])
-    return pathlib.Path(root) / TalePathMapper().davToPhysical('/' + taleId)[1:]
 
 
 def getTaleDirPath(tale: dict, rootProp: str) -> pathlib.Path:
--- server/resources/version.py
+++ server/resources/version.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 
 import pymongo
 
